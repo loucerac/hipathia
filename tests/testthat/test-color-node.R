08d13fac5d665b0b68f0a370cbc275e688a5bda9
Carlos <loucerac@unican.es>
2017-12-26 11:40:12 +0100

    Use "is" to check inheritance and "indetical+sort" to check equality.

--- tests/testthat/test-color-node.R
+++ tests/testthat/test-color-node.R
@@ -12,11 +12,11 @@ colors_de <- node.color.per.de(results, mgi, sample_group, "Tumor", "Normal")
 
 test_that("Classes are correct", {
     expect_is(colors_de, "list")
-    expect_true(all(sapply(colors_de, class) == "character"))
+    expect_true(all(sapply(colors_de$colors, function(x) is(x, "character"))))
     expect_equal(sum(sapply(unlist(colors_de), is.null)), 0)
 })
 
 test_that("Names are correct", {
-    expect_equal(length(setdiff(names(colors_de), names(mgi$pathigraphs))), 0)
-    expect_equal(length(setdiff(names(mgi$pathigraphs), names(colors_de))), 0)
+    expect_identical(sort(names(colors_de$colors)),
+                     sort(names(mgi$pathigraphs)))
 })
